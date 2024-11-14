5a23f35c0e31f248d24a910c275fb8811dd2c050
amoureuxnonono <114449623+amoureuxnonono@users.noreply.github.com>
2024-11-14 19:14:00 +0800

    Create MintNode.sol

---
+++ token/contracts/MintNode.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SyrupPool is Ownable {
+   IERC20 token = IERC20(address(0x2F16386bB37709016023232523FF6d9DAF444BE3));
+   IMint minter = IMint(address(0x2F16386bB37709016023232523FF6d9DAF444BE3));
+  }
+
+
