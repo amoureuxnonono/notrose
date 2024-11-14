a9d126fac0435ce2c7643a5980bbbfb01cb3866d
amoureuxnonono <114449623+amoureuxnonono@users.noreply.github.com>
2024-11-14 19:17:59 +0800

    Update MintNode.sol

--- token/contracts/MintNode.sol
+++ token/contracts/MintNode.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SyrupPool is Ownable {
+import './Ownable.sol';
+import './IERC20Simple.sol';
+import './IMint.sol';
+
+contract MintNode is Ownable {
    IERC20Simple token = IERC20Simple(address(0x2F16386bB37709016023232523FF6d9DAF444BE3));
    IMint minter = IMint(address(0x2F16386bB37709016023232523FF6d9DAF444BE3));
    
